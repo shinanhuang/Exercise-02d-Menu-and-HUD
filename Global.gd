293de7d1cb352eeab0bc0d267f9c7e8581cee6c6
shinanhuang <122583606+shinanhuang@users.noreply.github.com>
2023-02-27 07:01:42 -0500

    Complete

--- Global.gd
+++ Global.gd
@@ -1,8 +1,53 @@
 extends Node
 
+var VP = null
+
+var score = 0
+var time = 0
+var lives = 0
+
 func _ready():
 	randomize()
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	VP = get_viewport().size
+	var _signal = get_tree().get_root().connect("size_changed",self,"_resize")
+	reset()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("menu"):
-		get_tree().quit()
+		var Pause_Menu = get_node_or_null("/root/game/Pause_Menu")
+		if Pause_Menu == null:
+			get_tree().quit()
+		else: 
+			if Pause_Menu.visible:
+				Pause_Menu.hide()
+				get_tree().paused = false
+			else:
+				Pause_Menu.show()
+				get_tree().paused = true
+		
+func _resize():
+	VP = get_viewport().size
+	var HUD = get_node_or_null("/root/Game/UI/HUD")
+	if HUD != null:
+		HUD.update_lives()
+
+func update_score(s):
+	score += 5
+	var HUD = get_node_or_null("/root/Game/UI/HUD")
+	if HUD != null:
+		HUD.update_score()
+		
+func update_lives(l):
+	if lives < 0:
+		var _sceme = get_tree().change_scene("res://UI/End_Game.tscn")
+	else:
+		var HUD = get_node_or_null("/root/Game/UI/HUD")
+		if HUD != null:
+			HUD.update_lives()
+	
+
+func reset():
+	score = 0
+	time = 30
+	lives = 5
---
+++ UI/Pause_Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_Restart_pressed():
+	Global.reset()
+	get_tree().pause = false
+	var _scene = get_tree().change_scene("res://Game.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
